8979fb4efa24a6fd3e6a2915b434e1f511c60427
Krzysztof Szromek <k.szromek@exlabs.pl>
2018-06-25 11:30:24 +0200

    progres

--- railsbuntu.sh
+++ railsbuntu.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 
 # Install and setup PostgreSQL ################################################
 
-print_step("Checking Postgres installation")
+print_step "Checking Postgres installation"
 if ! dpkg -s postgresql; then
   sudo apt-get install -y postgresql postgresql-contrib
   sudo service postgresql start
@@ -21,7 +21,7 @@ fi
 
 # Install Asdf ###############################################################
 
-print_step("Checking Asdf version manager")
+print_step "Checking Asdf version manager"
 if ! dpkg -s asdf; then
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.0
   echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
@@ -39,7 +39,7 @@ fi
 
 # Install Redis ###############################################################
 
-print_step("Checking Redis installation")
+print_step "Checking Redis installation"
 if ! dpkg -s redis-server; then
   sudo apt-get install -y redis-server
 else
@@ -48,7 +48,7 @@ fi
 
 # Install Node.js #############################################################
 
-print_step("Checking for Node.js")
+print_step "Checking for Node.js"
 if ! node --version; then
   asdf install nodejs 10.5.0
   asdf global nodejs 10.5.0
@@ -58,7 +58,7 @@ fi
 
 # Install Yarn #############################################################
 
-print_step("Checking for Yarn")
+print_step "Checking for Yarn"
 if ! yarn --v; then
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
@@ -70,7 +70,7 @@ fi
 
 # Ruby and Version Manager ####################################################
 
-print_step("Checking for Ruby")
+print_step "Checking for Ruby"
 if ! ruby -v; then
   asdf install ruby 2.5.1
   asdf global ruby 2.5.1
@@ -83,7 +83,7 @@ fi
 
 # Install Heroku CLI ##########################################################
 
-print_step("Checking for Heroku CLI")
+print_step "Checking for Heroku CLI"
 if ! heroku --version; then
   wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 else
@@ -92,7 +92,7 @@ fi
 
 # Install Ngrok exposer ######################################################
 
-print_step("Checking for Ngrok")
+print_step "Checking for Ngrok"
 if ! ngrok; then
   sudo apt install -y unzip
   wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
@@ -104,7 +104,7 @@ fi
 
 # Install Elasticsearch #################################################
 
-print_step("Elasticsearch")
+print_step "Elasticsearch"
 if ! systemctl is-active elasticsearch.service; then
   sudo apt-get install default-jre
   wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.3.1/elasticsearch-2.3.1.deb
@@ -118,5 +118,5 @@ fi
 
 # Cleaning up #################################################################
 
-print_step("Removing unused software...")
+print_step "Removing unused software..."
 sudo apt autoremove -y
